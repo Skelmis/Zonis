426bd437988fe955463539b0fe9cafdb124ca3b4
skelmis <ethan@koldfusion.xyz>
2023-03-10 20:20:31 +1300

    doc: fix docs

--- zonis/__init__.py
+++ zonis/__init__.py
@@ -3,4 +3,19 @@ from .exceptions import *
 from .server import Server
 from .client import Client, route
 
+__all__ = (
+    "Packet",
+    "RequestPacket",
+    "Server",
+    "Client",
+    "route",
+    "BaseZonisException",
+    "DuplicateConnection",
+    "DuplicateRoute",
+    "UnhandledWebsocketType",
+    "UnknownRoute",
+    "UnknownClient",
+    "RequestFailed",
+)
+
 __version__ = "1.3.0"
